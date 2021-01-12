705bc05af1ab3857e3ad5cbc7da1e8e2a0cc19fb
pavle <markovic.pavle@outlook.com>
2021-01-12 20:30:17 +0100

    Optimize code to work with whole dataset.

--- assignment_4/model_1.py
+++ assignment_4/model_1.py
@@ -1,8 +1,7 @@
 import sys
-import copy
 import time
 import argparse
-import random
+from collections import defaultdict
 
 
 def main(args):
@@ -10,73 +9,78 @@ def main(args):
     source_path = args.data + '.' + args.e
     target_path = args.data + '.' + args.f
 
-    count_source_words = dict()
-    count_target_source = dict()
-    probs_target_source = dict()
+    source_count = 0
+    target_count = 0
+    source2ind = dict()
+    target2ind = dict()
     bitext = list()
 
     with open(source_path, 'r', encoding="utf-8") as source_file, open(target_path, 'r', encoding="utf-8") as target_file:
         for pair, (source_sent, target_sent) in enumerate(zip(source_file, target_file)):
-            source_sent_words = source_sent.strip().split()
-            target_sent_words = target_sent.strip().split()
+            source_sent_words = list()
+            target_sent_words = list()
+
+            for source_word in source_sent.strip().split():
+                if source_word not in source2ind:
+                    source2ind[source_word] = source_count
+                    source_count += 1
+                source_sent_words.append(source2ind[source_word])
+
+            for target_word in target_sent.strip().split():
+                if target_word not in target2ind:
+                    target2ind[target_word] = target_count
+                    target_count += 1
+                target_sent_words.append(target2ind[target_word])
+
             bitext.append([source_sent_words, target_sent_words])
-
-            for word in source_sent_words:
-                if word not in count_source_words:
-                    count_source_words[word] = 0
-
-            for word in target_sent_words:
-                if word not in count_target_source:
-                    count_target_source[word] = 0
-                    probs_target_source[word] = 0
-
+                    
             if pair == args.n-1:
                 break
 
-    target_voc_size = len(probs_target_source)
-    probs_target_source = {key: copy.deepcopy({k: 1/target_voc_size for k in count_source_words.keys()}) for key in probs_target_source.keys()}
-
-    #word = random.choice(list(count_source_words.keys()))
-    #total_sum = sum([value[word] for value in probs_target_source.values()])
+    probs_target_source = defaultdict(lambda: 1/target_count)
        
     # EM training
+    em_start = time.time()
     for iter in range(args.i):
         # All counts to zero
-        count_source_words = {key: 0 for key in count_source_words.keys()}
-        count_target_source = {key: copy.deepcopy(count_source_words) for key in count_target_source.keys()}
-
+        count_source_words = defaultdict(float)
+        count_target_source = defaultdict(float)
+        
         # E-step
         for example, (source_sent, target_sent) in enumerate(bitext):
             for target_word in target_sent:
                 # Calculate value for normalization term
-                norm_term = sum([probs_target_source[target_word][source_word] for source_word in source_sent])
+                norm_term = sum([probs_target_source[(target_word, source_word)] for source_word in source_sent])
 
                 for source_word in source_sent:
                     # Calculate expected count
-                    expected_count = probs_target_source[target_word][source_word] / norm_term
-                    count_target_source[target_word][source_word] += expected_count
+                    expected_count = probs_target_source[(target_word, source_word)] / norm_term
+                    count_target_source[(target_word, source_word)] += expected_count
                     count_source_words[source_word] += expected_count
 
         # M-step
-        for target_word in probs_target_source.keys():
-            for source_word in count_source_words.keys():
-                probs_target_source[target_word][source_word] = count_target_source[target_word][source_word] / count_source_words[source_word]
+        probs_target_source.update({key: count_target_source[key]/count_source_words[key[1]] for key in probs_target_source.keys()})
+
+    em_end = time.time()
 
     # Choosing alignments
+    alignment_start = time.time()
     for example, (source_sent, target_sent) in enumerate(bitext):
-        alignment = list()
-
         for target_pos, target_word in enumerate(target_sent):
             best_prob = 0
             best_position = 0
 
             for position, source_word in enumerate(source_sent):
-                if probs_target_source[target_word][source_word] > best_prob:
-                    best_prob = probs_target_source[target_word][source_word]
+                if probs_target_source[(target_word, source_word)] > best_prob:
+                    best_prob = probs_target_source[(target_word, source_word)]
                     best_position = position
 
-            sys.stdout.write("%i-%i " % (target_pos, best_position))
-        sys.stdout.write("\n")
+            #sys.stdout.write("%i-%i " % (target_pos, best_position))
+        #sys.stdout.write("\n")
+
+    alignment_end = time.time()
+    print(f"EM training time: {em_end - em_start}")
+    print(f"Alignment time: {alignment_end - alignment_start}")
 
 
 if __name__ == "__main__":
@@ -86,9 +90,8 @@ if __name__ == "__main__":
     parser.add_argument("-f", default="f", type=str, help="Suffix of Target lang filename (default=f)")
     parser.add_argument("-i", default=10, type=int, help="Number of iteration for EM algorithm (default=10)")
     parser.add_argument("-n", default=100000000000, type=int, help="Number of sentences to use for training and alignment")
-    #parser.add_argument("-draw", default=False, action='store_true', help="Flag to indicate if parse trees should be print or draw. Omit if don't want.")
     args = parser.parse_args()
 
     start_time = time.time()
     main(args)
-    end_time = time.time() - start_time
+    print(f"Total time: {time.time() - start_time}")
